83cdd818a9f7cfb9c18fbadfbe555f0cb23addf7
mre55 <misganawmihreteab>
2022-04-11 23:00:53 +0300

    Add confirmation email for the forget password

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.gmail.com',
+  domain: 'gmail.com',
+  port: 465,
+  authentication: 'plain',
+  :ssl => true,
+  :tsl => true,
+  enable_starttls_auto: true,
+  user_name: Rails.application.credentials.email[:user_name],
+  password: Rails.application.credentials.email[:password]}

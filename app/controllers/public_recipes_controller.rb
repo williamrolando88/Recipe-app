b8ade1185771e43bb3195e81ad68b680fd4abf64
williamrolando88 <william.rolando88@hotmail.com>
2022-04-12 15:56:48 -0500

    Add `public_recipes` controller

---
+++ app/controllers/public_recipes_controller.rb
@@ -0,0 +1,10 @@
+class PublicRecipesController < ApplicationController
+
+  def index
+    @recipes = Recipe.where(public: true)
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+end

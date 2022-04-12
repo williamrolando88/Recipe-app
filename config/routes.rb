23217498fac3ebcde057b34ce8e4aae93edb0929
williamrolando88 <william.rolando88@hotmail.com>
2022-04-12 11:08:32 -0500

    Add `current_user` access for all models

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_for :users, skip: :all
+
   resources :recipes, except: [:update, :edit]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "recipes#index"
+
 end

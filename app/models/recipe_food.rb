ffba541e7c90ddcf1d21955af958364829396a27
williamrolando88 <william.rolando88@hotmail.com>
2022-04-14 16:22:34 -0500

    Fix total ingredients cost not showing for public recipes

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -3,8 +3,10 @@ class RecipeFood < ApplicationRecord
   belongs_to :food
 
   def self.total_ingredients_cost(recipe_id)
-    # recipes = RecipeFood.where(recipe_id:)
-    # recipes.map { |recipe| recipe.quantity * recipe.food.price }.sum.round(2)
+    recipes = RecipeFood.where(recipe_id:)
+    return 0 if recipes.empty?
+
+    recipes.map { |recipe| recipe.quantity * recipe.food.price }.sum.round(2)
   end
 
   def self.total_ingredients(recipe_id)

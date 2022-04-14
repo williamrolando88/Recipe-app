b595757d6a41c3713cfe40b2282e0208e97653a1
mre55 <misganawmihreteab>
2022-04-14 23:51:32 +0300

    Edit Food page test

--- spec/food_spec.rb
+++ spec/food_spec.rb
@@ -1,31 +1,28 @@
 require 'rails_helper'
+require 'cancan/matchers'
 
-RSpec.describe 'Food page', type: :feature do
+RSpec.describe Food, type: :model do
   before :each do
-    @user1 = User.new(name: 'Mre', email: 'mre@gmail.com', password: '123456')
-    @user1.save
-    visit new_user_session_path
-    fill_in 'Email', with: @user1.email
-    fill_in 'Password', with: @user1.password
-    click_button 'Log in'
-    visit root_path
+    @user1 = User.create(name: 'Mre', email:'mre@gmail.com', password:'123456')
+    @user1.save!
+    @food = Food.new(name:'Apple', measurement_unit:'Unit', price:55)
   end
 
-  it 'should be able to see Foods' do
-    expect(page).to have_content('Public Recipes')
+  it 'should not be null' do
+    expect(@food).to_not be_valid
   end
 
-  it 'should be able to redirect to add food route' do
-    click_link 'My foods'
-    expect(page).to have_current_path(foods_path)
+  it 'should pass if type is String' do
+    expect(@food.name).to be_a(String)
   end
 
-  it 'should be able to see save food' do
-    visit new_food_path
-    fill_in 'Name', with: 'Pizza'
-    fill_in 'Measurement unit', with: 'grams'
-    fill_in 'Price', with: '10'
-    click_button 'Add food'
-    expect(page).to have_content('Pizza')
+  it 'should fail if type is Numeric' do
+    @food.name = 1
+    expect(@food.name).not_to be_a(Numeric)
+  end
+
+  it 'should be valid' do
+    @food.user_id = @user1.id
+    expect(@food).to be_valid
   end
 end
